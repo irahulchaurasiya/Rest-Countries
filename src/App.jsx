336b83ea90acee005228372e74d92059063782d9
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-10-26 23:09:57 +0530

    Add app jsx file

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Homepage from "./pages/Homepage";
+import CountryPage from "./pages/SingleCountryPage";
+import { ThemeProvider } from "./context/ThemeContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const App = () => {
+  return (
+    <>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="/detail/:cca3" element={<CountryPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
