da915ad9c7cba6785b6a558c8ca2afded4c2aa42
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-10-28 09:26:15 +0530

    Changed semantic tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
   const { darkMode, toggleTheme } = useTheme();
 
   return (
-    <div
-      className={`flex flex-col pb-0 sm:flex-row place-content-between align-center pb-5 p-10 drop-shadow-md sm:pl-16 sm:pr-16 ${
+    <header
+      className={`flex pb-0 sm:flex-row place-content-between align-center pb-5 p-10 drop-shadow-md sm:pl-16 sm:pr-16 ${
         darkMode
           ? "bg-veryDarkBlueDMB text-whiteDMTLME"
           : "bg-whiteDMTLME text-veryDarkBlueLMT"
       }`}
     >
       <div className="inline-block text-xl font-bold">Where in the world?</div>
-      <div className="flex pt-4 sm:pt-0">
+      <div className="flex">
         <div className="flex pt-1" onClick={toggleTheme}>
           <img
             className="h-5 w-5 cursor-pointer"
@@ -26,7 +26,7 @@ const Header = () => {
           {darkMode ? "Light Mode" : "Dark Mode"}
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
