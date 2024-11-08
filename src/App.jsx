34cf4b86067acaf963f9e791196494f65939c5af
Rahul <rahul.chaurasiya.29.3@mountblue.tech>
2024-11-08 12:18:29 +0530

    modify Single country path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Homepage from "./pages/Homepage";
-import CountryPage from "./pages/SingleCountryPage";
-import { ThemeProvider } from "./context/ThemeContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import SingleCountryPage from "./pages/SingleCountryPage";
+import { ThemeProvider } from "./context/ThemeContext";
 import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
 
@@ -13,7 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainLayout />}>
               <Route index element={<Homepage />} />
-              <Route path="/detail/:cca3" element={<CountryPage />} />
+              <Route path="/detail/:cca3" element={<SingleCountryPage />} />
               <Route path="*" element={<NotFoundPage />} />
             </Route>
           </Routes>
